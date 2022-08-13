620f23ca3c52c7106ee66a3eac2a44dffeb6aae2
shakirabugarbia <shakir.abugarbia@gmail.com>
2022-08-13 10:39:37 +0300

    adding tailwind css file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import "./index.css";
 
 function App() {
-  return <div className="App">Welcome APP</div>;
+  return <div className="text-3xl font-bold underline">Welcome APP</div>;
 }
 
 export default App;
